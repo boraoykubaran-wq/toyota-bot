dfa86ee3c41a3e68d1f4f3b41ca06c2a61bb701b
boraoykubaran-wq <boraoykubaran@gmail.com>
2026-03-28 18:47:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import os
 
 URL = "https://www.sahibinden.com/oto360/sifir-araclar/toyota-corolla-cross-fiyat-listesi"
 
@@ -12,34 +13,14 @@ def get_price():
     response = requests.get(URL, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    text = soup.get_text()
-
+    text = soup.get_text(separator="\n")
     lines = text.split("\n")
 
     for i, line in enumerate(lines):
-        if "Hybrid Flame e-CVT" in line:
-
-            # alt satırda fiyat var
+        if "Corolla Cross SUV" in line:
+            # alt satırlarda fiyatı ara
             for j in range(i, i+5):
                 if "TL" in lines[j]:
-                    return f"{line.strip()} → {lines[j].strip()}"
+                    return lines[j].strip()
 
     return None
-
-
-last_price = None
-
-while True:
-    try:
-        price = get_price()
-        print("Bulunan fiyat:", price)
-
-        if last_price and price != last_price:
-            print("🚨 FİYAT DEĞİŞTİ!")
-
-        last_price = price
-        time.sleep(300)
-
-    except Exception as e:
-        print("Hata:", e)
-        time.sleep(60)
